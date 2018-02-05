e90498076f5ae4784862f3d870b3540d9eaa1904
Cheng <cy699@nyu.edu>
2018-02-05 13:30:22 -0500

    Update hw1_q1.py

--- hw1_q1.py
+++ hw1_q1.py
@@ -1,55 +1,56 @@
+#!/opt/anaconda/bin/python
 from scipy.io import loadmat
 import numpy as np 
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import random
-
-def min_idx(seq):
-    it_obj = iter(seq)
-    res = 0
-    try: 
-        min_num = it_obj.next()
-    except StopIteration: 
-        raise ValueError("the sequence is empty")
-        
-    for i,item in enumerate(it_obj,start=1):
-        if item > min_num:
-            min_num = item
-            res = i
-    return res
+import multiprocessing as mp
 
 def calc_func(data_feature, test_feature, data_labels, test_labels):
-    arg2 = -2 * np.dot(data_feature, np.transpose(test_feature))
-    arg1 = (test_feature * test_feature).sum(axis=1)[np.newaxis,:]
-    arg3 = (data_feature * data_feature).sum(axis=1)[:,np.newaxis]
+  arg2 = -2 * np.dot(data_feature, np.transpose(test_feature))
+  arg1 = (test_feature * test_feature).sum(axis=1)[np.newaxis,:]
+  arg3 = (data_feature * data_feature).sum(axis=1)[:,np.newaxis]
+  dist = arg1 + arg2 + arg3
+
+  res_idx = [np.argmin(line) for line in dist]
+  res = data_labels - test_labels[res_idx]
+
+  return res[np.where(res == 0)].shape[0]
+ 
+def _worker(shared_dict, raw_data, training_size, num_iter=10):
+  if training_size > len(raw_data['data']):
+    raise Exception('Training size can\'t larger than data size')
+
+  for _ in xrange(num_iter):
+    total_size = len(raw_data['data'])
+    sel_data = random.sample(xrange(total_size), training_size)
+    data_feature = raw_data['data'][sel_data].astype('float')
+    data_labels = raw_data['labels'][sel_data].astype('float')
+    test_feature = raw_data['testdata'].astype('float')
+    test_labels =  raw_data['testlabels'].astype('float')
+
+    res = calc_func(data_feature, test_feature, data_labels, test_labels)    
+
+    shared_dict[training_size] = shared_dict.get(training_size, []) + [res]
+
+def calc_data(raw_data, training_set=None, num_iter=10):
+    if not training_set:
+      training_set = (1000, 2000, 4000, 8000)
     
-    dist = arg1 + arg2 + arg3
+    shared_dict = mp.Manager().dict()
+    jobs = []
 
-    res_idx = [min_idx(line) for line in dist]
-    res = data_labels - test_labels[res_idx]
+    for num in training_set:
+      process = mp.Process(target=_worker, args=(shared_dict, raw_data, num, num_iter))
+      process.start()
+      jobs.append(process)
 
-    return res
+    [p.join() for p in jobs]
 
-def calc_data(raw_data):
-    nums = (1000, 2000, 4000, 8000)
-    res_dist = {}
-    for _ in xrange(10):
-        for num in nums:
-            total_size = len(raw_data['data'])
-            sel_data = random.sample(xrange(total_size), num)
-            data_feature = raw_data['data'][sel_data].astype('float')
-            data_labels = raw_data['labels'][sel_data].astype('float')
-            test_feature =  raw_data['testdata'].astype('float')
-            test_labels =  raw_data['testlabels'].astype('float')
-
-            res = calc_func(data_feature, test_feature, data_labels, test_labels)    
-
-            res_dist[num] = res_dist.get(num, []) + [res[np.where(res == 0)].shape[0]]
-
-    for k, v in res_dist.iteritems():
-        print 'samples size: {} avg_mean: {} all_iter_data:{}'.format(k, sum(v)/len(v), v)
+    for idx in training_set:
+      print 'samples size: {} avg_mean: {} all_iter_data:{}'.format(idx, np.mean(shared_dict[idx])/idx, shared_dict[idx])
     
-    return res_dist.values()
+    return shared_dict
         
 def plot_result(data):
     x_axis = range(len(data))
