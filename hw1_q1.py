5332728decf685a795580c1e9ebc9419047c3403
Cheng <cy699@nyu.edu>
2018-02-05 13:41:16 -0500

    Update hw1_q1.py

--- hw1_q1.py
+++ hw1_q1.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import random
 import multiprocessing as mp
+import pandas as pd
 
 def calc_func(data_feature, test_feature, data_labels, test_labels):
   arg2 = -2 * np.dot(data_feature, np.transpose(test_feature))
@@ -17,23 +18,24 @@ def calc_func(data_feature, test_feature, data_labels, test_labels):
 
   return res[np.where(res == 0)].shape[0]
  
-def _worker(shared_dict, raw_data, training_size, num_iter=10):
+def _worker(shared_dict, raw_data, training_size, labels, num_iter=10):
   if training_size > len(raw_data['data']):
     raise Exception('Training size can\'t larger than data size')
 
+  train_data, train_label, test_data, test_label = labels
   for _ in xrange(num_iter):
     total_size = len(raw_data['data'])
     sel_data = random.sample(xrange(total_size), training_size)
-    data_feature = raw_data['data'][sel_data].astype('float')
-    data_labels = raw_data['labels'][sel_data].astype('float')
-    test_feature = raw_data['testdata'].astype('float')
-    test_labels =  raw_data['testlabels'].astype('float')
+    data_feature = raw_data[train_data][sel_data].astype('float')
+    data_labels = raw_data[train_label][sel_data].astype('float')
+    test_feature = raw_data[test_data].astype('float')
+    test_labels = raw_data[test_label].astype('float')
 
     res = calc_func(data_feature, test_feature, data_labels, test_labels)    
 
     shared_dict[training_size] = shared_dict.get(training_size, []) + [res]
 
-def calc_data(raw_data, training_set=None, num_iter=10):
+def calc_data(raw_data, labels, training_set=None, num_iter=10):
     if not training_set:
       training_set = (1000, 2000, 4000, 8000)
     
@@ -41,16 +43,19 @@ def calc_data(raw_data, training_set=None, num_iter=10):
     jobs = []
 
     for num in training_set:
-      process = mp.Process(target=_worker, args=(shared_dict, raw_data, num, num_iter))
+      process = mp.Process(target=_worker, args=(shared_dict, raw_data, num, labels, num_iter))
       process.start()
       jobs.append(process)
 
     [p.join() for p in jobs]
 
+    res = []
     for idx in training_set:
-      print 'samples size: {} avg_mean: {} all_iter_data:{}'.format(idx, np.mean(shared_dict[idx])/idx, shared_dict[idx])
-    
-    return shared_dict
+      value = shared_dict[idx]
+      res.append([idx, np.mean(value)/idx, np.std(value)])
+
+    res_df = pd.DataFrame(res, columns=['Iter_Num', 'Mean', 'STD'])
+    return res_df
         
 def plot_result(data):
     x_axis = range(len(data))
@@ -62,7 +67,13 @@ def plot_result(data):
 
 def run():
     ocr = loadmat('ocr.mat')
-    calc_data(ocr)
+    print 'Using test data:'
+    labels = ['data', 'labels', 'testdata', 'testlabels']
+    print calc_data(ocr, labels)
+
+    print 'Using test data:'
+    labels = ['data', 'labels', 'data', 'labels']
+    print calc_data(ocr, labels)
 
 if  __name__ == '__main__':
     run()
