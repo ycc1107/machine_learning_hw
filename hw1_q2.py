29316c9bc4f192efcff3877484356cae5f9f3c3c
Cheng <cy699@nyu.edu>
2018-02-05 20:16:42 -0500

    Update hw1_q2.py

--- hw1_q2.py
+++ hw1_q2.py
@@ -29,26 +29,25 @@ def banch_mark(wine):
 def customized_pre_model(wine):
     regr = LinearRegression()
     data = wine['data']
-    real_y = wine['testlabels']
-    test_data = wine['testdata']
     train_data_y = wine['labels']
     
     min_score = 10e10
-    for a, b, c  in combinations(range(len(data[0])), r=3):
-        temp = np.column_stack((data[:, a], data[:, b], data[:, c]))
-        test_data_tmp = np.column_stack((test_data[:, a], test_data[:, b], test_data[:, c]))
+
+    for i in range(1,4):
+      for item in combinations(range(1,len(data[0])), r=i):
+        temp = data[:,[0]+list(item)]
         regr.fit(temp, train_data_y)
-        pred_y = regr.predict(test_data_tmp)
-        loss = mean_squared_error(pred_y ,real_y)
-        score = regr.score(test_data_tmp, real_y)
+        pred_y = regr.predict(temp)
+        loss = mean_squared_error(pred_y ,train_data_y)
+
         if loss < min_score:
             min_score = loss
-            best_ = loss
-            idx_ = '{}.{}.{}'.format(a, b, c)
+            best_ =  loss
+            idx_ = '.'.join(map(str,item))
             print idx_ ,score, loss
             coef = regr.coef_
 
-    res_df = pd.DataFrame([coef[0]], columns=[mapping[i] for i in idx_.split('.')])
+    res_df = pd.DataFrame([coef[0]], columns=['Unknown']+[mapping[i] for i in idx_.split('.')])
     res_df.index = ['coefficient']
 
     return res_df
